783459f345973d20052212f2b2dd4ce2cefef15c
unknown <sajmon88@protonmail.ch>
2021-03-30 00:45:13 +0200

    Removed legacy testing paths

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,10 @@
 import { Application } from 'express'
 
-import authenticate from '../middlewares/authenticate'
-
 import auth from './auth'
 
 const prefix = '/api/v1'
 
 export default function(app: Application) {
-
-    app.get(prefix + '/users', authenticate, (req, res) => {
-        const users = [{id: 1, name: 'Adam'}]
-
-        res.send(users)
-    })
 
     app.use(prefix + '/auth', auth)
 
